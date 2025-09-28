ece8fada42bd6ce34e3d5acae21f7663bc5ea06c
GallonEmilien <gallonemilien@gmail.com>
2025-09-28 15:30:34 +0200

    chore: remove debug logs

--- common/src/main/java/fr/gallonemilien/mixin/LivingEntityMixin.java
+++ common/src/main/java/fr/gallonemilien/mixin/LivingEntityMixin.java
@@ -27,16 +27,10 @@ public abstract class LivingEntityMixin {
 
     @Inject(method = "causeFallDamage", at = @At("HEAD"), cancellable = true)
     private void reduceHorseFallDamage(double distance, float multiplier, DamageSource source, CallbackInfoReturnable<Boolean> cir) {
-        System.out.println("CAUSE FALL DAMAGE?"+ ((Object) this).getClass());
         if ((Object) this instanceof DopedHorseEntity dopedHorseEntity) {
-            System.out.println(dopedHorseEntity.getShoeContainer().getItem(0));
             if (!dopedHorseEntity.getShoeContainer().getItem(0).isEmpty() &&
                     dopedHorseEntity.getShoeContainer().getItem(0).getItem() instanceof ShoeItem shoeItem) {
                 double jumpBonus = shoeItem.getJumpModifier();
-
-                System.out.println(multiplier+ " DE BASE");
-                System.out.println(jumpBonus+ " BONUS");
-                System.out.println(Math.max(0f, multiplier - (float)jumpBonus)+ " MULTIPLIER FINAL");
                 float newMultiplier = Math.max(0f, multiplier - (float)jumpBonus);
                 boolean result = ((LivingEntity)(Object) this).causeFallDamage(distance, newMultiplier, source);
                 cir.setReturnValue(result);
