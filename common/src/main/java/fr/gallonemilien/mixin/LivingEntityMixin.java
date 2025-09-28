25fbff8a4a9c7736256e045004c0973c0fc61caf
GallonEmilien <gallonemilien@gmail.com>
2025-09-28 15:30:01 +0200

    feat: avoid an horse from moving when saddled and dismounted

--- common/src/main/java/fr/gallonemilien/mixin/LivingEntityMixin.java
+++ common/src/main/java/fr/gallonemilien/mixin/LivingEntityMixin.java
@@ -4,13 +4,26 @@ import fr.gallonemilien.items.ShoeItem;
 import fr.gallonemilien.persistence.DopedHorseEntity;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.horse.AbstractHorse;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
+
+    //Avoid from moving when the horse is saddled
+    @Inject(method = "travel", at=@At("HEAD"), cancellable = true)
+    private void travel(Vec3 vec3, CallbackInfo ci) {
+        if(((Object) this) instanceof AbstractHorse horse) {
+            if (!horse.isVehicle() && horse.isSaddled()) {
+                ci.cancel();
+            }
+        }
+    }
 
     @Inject(method = "causeFallDamage", at = @At("HEAD"), cancellable = true)
     private void reduceHorseFallDamage(double distance, float multiplier, DamageSource source, CallbackInfoReturnable<Boolean> cir) {
